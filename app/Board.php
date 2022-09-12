cd2acaf058250b283e7200b040ef0e3cae28ecff
Anuzky <anuzky@live.com.ar>
2022-09-12 15:07:54 -0300

    Prototipos funcionales de Token y Board

--- app/Board.php
+++ app/Board.php
@@ -1,25 +1,39 @@
 <?php
 namespace App;
+
+//use App\Token;
 // Tablero del juego
 
-class GameBoard {
-  protected $empty = True;
-  protected $contents = [];
+class Board
+{
+  protected $contents =  array(
+    1 => array('', '', '', '', '', '', 0), 
+    2 => array('', '', '', '', '', '', 0), 
+    3 => array('', '', '', '', '', '', 0), 
+    4 => array('', '', '', '', '', '', 0), 
+    5 => array('', '', '', '', '', '', 0), 
+    6 => array('', '', '', '', '', '', 0),
+    7 => array('', '', '', '', '', '', 0)
+  );
 
   public function __construct() {
   }
 
-  public function isEmpty() {
-    return $empty;
+  public function addToken(Token $token, int $col) {
+    $index = $this->contents[$col][6];
+    $this->contents[$col][$index] = $token;
+    $this->contents[$col][6]++;
   }
 
-  public function addToken(GameToken $token, $column) {
-    array_push($this->contents[$column], $token);
-    this->$empty = False;
-  }
-
-  public function clearUp() {
-    this->$contents = [];
-    this->$empty = True;
+  public function printBoard() {
+    for ($row = 5; $row >= 0; $row--) {
+      for ($col = 1; $col <=7; $col++) {
+        if ($this->contents[$col][$row])
+          $this->contents[$col][$row]->printToken();
+        else
+          print('0 ');
+      }
+      print("\n");
+    }
   }
 }
--- app/Token.php
+++ app/Token.php
@@ -2,11 +2,18 @@
 namespace App;
 // Fichas del juego
 
-class GameToken {
+class Token {
   protected $color;
 
   public function __construct($color){
-    this->color = $color;
+    $this->color = $color;
+  }
+
+  public function printToken(){
+    if ($this->color == "Red")
+      print('R ');
+    else
+      print('B ');
   }
 
 }
