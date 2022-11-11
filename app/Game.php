9a2982239af917eda243fb4eda921c762a96ccfc
Anuzky <anuzky@live.com.ar>
2022-11-11 18:19:49 -0300

    Ahora el juego termina si no hay espacio

--- app/Game.php
+++ app/Game.php
@@ -23,18 +23,23 @@ class Game
     $this->current_player = "1";
     $game_status = new GameStatus();
     $printer = new BoardPrinter();
+    $tokenQty = 0;
 
-    while ($game_status->gameEnded($board) == false) {
+    while ($game_status->gameEnded($board) == false and $tokenQty < 42) {
       print("Jugador {$this->current_player}, tu turno. Ingresa una columna: ");
       fscanf(STDIN, "%d", $pos);
       $board->addToken(new Token($this->current_color), $pos);
+      $tokenQty += 1;
 
       $printer->printBoard($board);
 
       $this->nextTurn();
     }
-    
-    $this->nextTurn();
-    print("Felicitaciones, ha ganado el jugador {$this->current_player}!");
+    if ($tokenQty == 42)
+      print("El tablero se llenó y nadie ganó.");
+    else {
+      $this->nextTurn();
+      print("Felicitaciones, ha ganado el jugador {$this->current_player}!");
+    }
   }
 }
